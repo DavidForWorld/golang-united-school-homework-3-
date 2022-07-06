b9c84c7e52e1d5ceb7c9d6b0269e53a857a7b665
DAV <99496664+DavidForWorld@users.noreply.github.com>
2022-07-06 20:32:21 +0400

    One day it will work

--- task01-arrays.go
+++ task01-arrays.go
@@ -4,7 +4,7 @@ func average(input [15]float32) (result float32) {
 	array := []int{1, 2, 3, 4, 5, 6}
 	N := 6
 	sum := 0
-	for i := 0; i < n; i++ {
+	for i := 0; i < N; i++ {
 		sum += (array[i])
 	}
 	avg := (float32(sum)) / (float32(N))
--- task03-map.go
+++ task03-map.go
@@ -5,16 +5,16 @@ import (
 )
 
 func sortMapValues(input map[int]string) (result []string) {
-	Input := map[string]int{10: "aa", 0: "bb", 500: "cc"}
+	m := map[string]int{"aa": 10, "bb": 0, "cc": 500}
 
-	keys := make([]string, 0, len(Input))
-
-	for key := range Input {
-		keys = append(keys, key)
+	keys := make([]string, 0, len(m))
+	for k := range m {
+		keys = append(keys, k)
 	}
+	sort.Strings(keys)
 
-	sort.SliceStable(keys, func(i, j int) bool {
-		return Input[keys[i]] < Input[keys[j]]
-	})
+	for _, k := range keys {
+
+	}
 	return
 }
